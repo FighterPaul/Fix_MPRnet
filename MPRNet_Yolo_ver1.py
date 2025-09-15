3d6130f08857d351e1c7cc39934f606bfee66978
FighterPaul <racha.tungtrakool@gmail.com>
2025-09-16 02:51:19 +0700

    can dynamic quantize MPRNet

--- MPRNet_Yolo_ver1.py
+++ MPRNet_Yolo_ver1.py
@@ -16,6 +16,7 @@ import cv2
 import argparse
 import time
 from torchinfo import summary
+from torch.ao.quantization import quantize_dynamic
 # from torchsummary import summary
 
 
@@ -61,30 +62,159 @@ def prune_model_YOLO(model, amount=0.2):
 def prune_model_MPRNet(model, amount=0.2):
     for module in model.modules():
         if isinstance(module, torch.nn.Sequential):
-            print("This module is Sequential", end='  ')
+            # print("This module is Sequential", end='  ')
             for sub_module in module:
                 if isinstance(sub_module, torch.nn.Linear):
-                    print(" sub module is Linear ")
+                    # print(" sub module is Linear ")
                     prune.l1_unstructured(sub_module, name='weight', amount=amount)
                     prune.remove(module=sub_module, name='weight')
                 elif isinstance(sub_module, torch.nn.Conv2d):
-                    print(" sub module is Conv ")
+                    # print(" sub module is Conv ")
                     prune.l1_unstructured(sub_module, name='weight', amount=amount)
                     prune.remove(module=sub_module, name='weight')
 
 
         elif isinstance(module, torch.nn.Linear):
-            print("This module is Linear")
+            # print("This module is Linear")
             prune.l1_unstructured(module, name='weight', amount=amount)
             prune.remove(module=module, name='weight')
         elif isinstance(module, torch.nn.Conv2d):
-            print("This module is Conv")
+            # print("This module is Conv")
             prune.l1_unstructured(module, name='weight', amount=amount)
             prune.remove(module=module, name='weight')
         else:
-            print("This module is something else...")
+            # print("This module is something else...")
+            pass
 
     return model
+
+
+
+# @title FLOPS computation
+# Code from https://github.com/Eric-mingjie/rethinking-network-pruning/blob/master/imagenet/l1-norm-pruning/compute_flops.py
+import numpy as np
+import os
+
+import torch
+import torchvision
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+def print_model_param_nums(model=None):
+    if model == None:
+        model = torchvision.models.alexnet()
+    total = sum([param.nelement() if param.requires_grad else 0 for param in model.parameters()])
+    print('  + Number of params: %.4fM' % (total / 1e6))
+
+def count_model_param_flops(model=None, input_res_width=480, input_res_height=320, multiply_adds=True):
+
+    prods = {}
+    def save_hook(name):
+        def hook_per(self, input, output):
+            prods[name] = np.prod(input[0].shape)
+        return hook_per
+
+    list_1=[]
+    def simple_hook(self, input, output):
+        list_1.append(np.prod(input[0].shape))
+    list_2={}
+    def simple_hook2(self, input, output):
+        list_2['names'] = np.prod(input[0].shape)
+
+
+    list_conv=[]
+    def conv_hook(self, input, output):
+        batch_size, input_channels, input_height, input_width = input[0].size()
+        output_channels, output_height, output_width = output[0].size()
+
+        kernel_ops = self.kernel_size[0] * self.kernel_size[1] * (self.in_channels / self.groups)
+        bias_ops = 1 if self.bias is not None else 0
+
+        params = output_channels * (kernel_ops + bias_ops)
+        # flops = (kernel_ops * (2 if multiply_adds else 1) + bias_ops) * output_channels * output_height * output_width * batch_size
+
+        num_weight_params = (self.weight.data != 0).float().sum()
+        flops = (num_weight_params * (2 if multiply_adds else 1) + bias_ops * output_channels) * output_height * output_width * batch_size
+
+        list_conv.append(flops)
+
+    list_linear=[]
+    def linear_hook(self, input, output):
+        batch_size = input[0].size(0) if input[0].dim() == 2 else 1
+
+        weight_ops = self.weight.nelement() * (2 if multiply_adds else 1)
+        bias_ops = self.bias.nelement()
+
+        flops = batch_size * (weight_ops + bias_ops)
+        list_linear.append(flops)
+
+    list_bn=[]
+    def bn_hook(self, input, output):
+        list_bn.append(input[0].nelement() * 2)
+
+    list_relu=[]
+    def relu_hook(self, input, output):
+        list_relu.append(input[0].nelement())
+
+    list_pooling=[]
+    def pooling_hook(self, input, output):
+        batch_size, input_channels, input_height, input_width = input[0].size()
+        output_channels, output_height, output_width = output[0].size()
+
+        kernel_ops = self.kernel_size * self.kernel_size
+        bias_ops = 0
+        params = 0
+        flops = (kernel_ops + bias_ops) * output_channels * output_height * output_width * batch_size
+
+        list_pooling.append(flops)
+
+    list_upsample=[]
+
+    # For bilinear upsample
+    def upsample_hook(self, input, output):
+        batch_size, input_channels, input_height, input_width = input[0].size()
+        output_channels, output_height, output_width = output[0].size()
+
+        flops = output_height * output_width * output_channels * batch_size * 12
+        list_upsample.append(flops)
+
+    def foo(net):
+        childrens = list(net.children())
+        if not childrens:
+            if isinstance(net, torch.nn.Conv2d):
+                net.register_forward_hook(conv_hook)
+            if isinstance(net, torch.nn.Linear):
+                net.register_forward_hook(linear_hook)
+            if isinstance(net, torch.nn.BatchNorm2d):
+                net.register_forward_hook(bn_hook)
+            if isinstance(net, torch.nn.ReLU):
+                net.register_forward_hook(relu_hook)
+            if isinstance(net, torch.nn.MaxPool2d) or isinstance(net, torch.nn.AvgPool2d):
+                net.register_forward_hook(pooling_hook)
+            if isinstance(net, torch.nn.Upsample):
+                net.register_forward_hook(upsample_hook)
+            return
+        for c in childrens:
+            foo(c)
+
+    if model == None:
+        model = torchvision.models.alexnet()
+    foo(model)
+    input = Variable(torch.rand(3,input_res_width,input_res_height).unsqueeze(0).to(model.parameters().__next__().device), requires_grad = True)
+    out = model(input)
+
+
+    total_flops = (sum(list_conv) + sum(list_linear) + sum(list_bn) + sum(list_relu) + sum(list_pooling) + sum(list_upsample))
+
+    print('Number of FLOPs: %.6f GFLOPs (%.2f MFLOPs)' % (total_flops / 1e9, total_flops / 1e6))
+
+    return total_flops
+
+
+def getfilesizeMB(path):
+    size_of_file = os.path.getsize(path) / (1024 * 1024)  # Size in MB
+    return size_of_file
 
 
 
@@ -122,7 +252,7 @@ LABELS = YOLO_MODEL.names
 
 print("Pruning YOLO Model ...")
 yolo_model_prepare_prune = YOLO_MODEL.model
-yolo_model_after_prune = prune_model_YOLO(model=yolo_model_prepare_prune, amount=0.3)
+yolo_model_after_prune = prune_model_YOLO(model=yolo_model_prepare_prune, amount=0.1)
 print("YOLO Model pruned")
 YOLO_MODEL.model = yolo_model_after_prune
 print("saving pruned YOLO model ....")
@@ -146,28 +276,59 @@ MPRNet_MODEL_WEIGHT = './model_deraining.pth'
 
 load_checkpoint(MPRNet_MODEL, MPRNet_MODEL_WEIGHT)
 
+# print(summary(MPRNet_MODEL, input_size=(1, 3, 480, 320)))
+count_model_param_flops(model=MPRNet_MODEL.eval(), input_res_width=480, input_res_height=320, multiply_adds=True)
+print(f"model file size {getfilesizeMB(path=MPRNet_MODEL_WEIGHT)} MB.")
+
 
 print("Pruning MPRNet Model ...")
 mprnet_model_prepare_prune = MPRNet_MODEL
-mprnet_model_after_prune = prune_model_MPRNet(model=mprnet_model_prepare_prune, amount=0.1)
+mprnet_model_after_prune = prune_model_MPRNet(model=mprnet_model_prepare_prune, amount=0.3)
 print("MPRNet Model pruned")
 MPRNet_MODEL = mprnet_model_after_prune
 print("saving pruned MPRNet model ....")
 torch.save(obj= MPRNet_MODEL.state_dict(), f='./MPRNet_trained_pruned.pth')
 print("Pruned MPRNet Model saved.")
 
+count_model_param_flops(model=MPRNet_MODEL.eval(), input_res_width=480, input_res_height=320, multiply_adds=True)
+print(f"model file size {getfilesizeMB(path='./MPRNet_trained_pruned.pth')} MB.")
+
+
+print("Quantizing MPRNet")
+load_file = run_path(os.path.join("MPRNet.py"))
+MPRNet_MODEL_PRUNED = load_file['MPRNet']()
+MPRNet_MODEL_PRUNED.cuda()
+MPRNet_MODEL_WEIGHT_PRUNED = './MPRNet_trained_pruned.pth'
+MPRNet_MODEL_PRUNED.load_state_dict(torch.load(f=MPRNet_MODEL_WEIGHT_PRUNED, weights_only=True))
+
+
+MPRNet_MODEL_QUANTIZED = quantize_dynamic(model=MPRNet_MODEL_PRUNED.cpu(),
+                 qconfig_spec={nn.Linear, nn.Conv2d, nn.Sequential},
+                 dtype=torch.qint8)
+print("MPRNet Model quantized")
+
+print("saving quantized MPRNet model ....")
+torch.save(obj= MPRNet_MODEL_QUANTIZED.state_dict(), f='./MPRNet_trained_quantized.pth')
+print("Quantized MPRNet Model saved.")
+
+
 
 
 print("Initilize MPRNet ....")
 load_file = run_path(os.path.join("MPRNet.py"))
-MPRNet_MODEL_PRUNED = load_file['MPRNet']()
-MPRNet_MODEL_PRUNED.cuda()
+MPRNet_MODEL_PRUNED_QUANTED = load_file['MPRNet']()
+MPRNet_MODEL_PRUNED_QUANTED.cuda()
 
 
-MPRNet_MODEL_WEIGHT_PRUNED = './MPRNet_trained_pruned.pth'
+MPRNet_MODEL_WEIGHT_PRUNED_QUANTED = './MPRNet_trained_quantized.pth'
 
-MPRNet_MODEL_PRUNED.load_state_dict(torch.load(f=MPRNet_MODEL_WEIGHT_PRUNED, weights_only=True))
-# load_checkpoint(MPRNet_MODEL_PRUNED, MPRNet_MODEL_WEIGHT_PRUNED)
+MPRNet_MODEL_PRUNED_QUANTED.load_state_dict(torch.load(f=MPRNet_MODEL_WEIGHT_PRUNED_QUANTED, weights_only=True))
+# load_checkpoint(MPRNet_MODEL_PRUNED_QUANTED, MPRNet_MODEL_WEIGHT_PRUNED)
+
+# print(summary(MPRNet_MODEL_PRUNED_QUANTED, input_size=(1, 3, 480, 320)))
+count_model_param_flops(model=MPRNet_MODEL_PRUNED_QUANTED.eval(), input_res_width=480, input_res_height=320, multiply_adds=True)
+print(f"model file size {getfilesizeMB(path=MPRNet_MODEL_WEIGHT_PRUNED_QUANTED)} MB.")
+
 
 
 
@@ -192,6 +353,7 @@ MPRNet_MODEL_PRUNED.load_state_dict(torch.load(f=MPRNet_MODEL_WEIGHT_PRUNED, wei
 
 # cap_arg = USB_IDX
 # cap = cv2.VideoCapture(cap_arg)
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 # cap.set(3, RES_W)
 # cap.set(4, RES_H)
 
@@ -205,16 +367,17 @@ images = natsorted(glob(os.path.join(input_dir, '*.jpg'))
 
 INTERMEDITE_FOLDER = './Images/EditDimension_images'
 for loop_idx, each_image in enumerate(images):
-    print(f"loop IDX :: {loop_idx}")
+    # print(f"loop IDX :: {loop_idx}")
     im = cv2.imread(each_image, cv2.IMREAD_COLOR)
     if im.shape[:2] != (480, 320):
-        print("found image wrong shape ::", im.shape[:2])
+        # print("found image wrong shape ::", im.shape[:2])
         im = cv2.resize(src=im, dsize=(480, 320), dst=None, fx= None, interpolation=cv2.INTER_LINEAR)
     else:
-        print("image good shape", im.shape[:2])
+        pass
+        # print("image good shape", im.shape[:2])
 
     path_to_save_overwirte = os.path.join(INTERMEDITE_FOLDER, os.path.basename(each_image))
-    print(f"saving at {path_to_save_overwirte}")
+    # print(f"saving at {path_to_save_overwirte}")
     cv2.imwrite(filename=path_to_save_overwirte, img=im)
 
 #-------------------------------------------------------------------------------
@@ -232,18 +395,18 @@ with torch.no_grad():
 
         print(f"Loop {loop_idx}")
     
-    # ret, frame = cap.read()
-    # if (frame is None) or (not ret):
-    #     print('Unable to read frames from the camera. This indicates the camera is disconnected or not working. Exiting program.')
-    #     break
+        # ret, frame = cap.read()
+        # if (frame is None) or (not ret):
+        #     print('Unable to read frames from the camera. This indicates the camera is disconnected or not working. Exiting program.')
+        #     break
 
-    # frame = cv2.resize(frame(RES_W, RES_H))
-    
-    # cv2.imshow(winname= 'USB Camera 00', mat=frame)
-    # key = cv2.waitKey(5)
+        # frame = cv2.resize(frame, (RES_W, RES_H))
+        
+        # cv2.imshow(winname= 'USB Camera 00', mat=frame)
+        # key = cv2.waitKey(5)
 
-    # if key ==('q') or key == ord('Q'):
-    #     break
+        # if key ==('q') or key == ord('Q'):
+        #     break
 
 
 #---------------------  Restoration -----------------------------
@@ -257,7 +420,7 @@ with torch.no_grad():
 
 
 
-        restored_image = MPRNet_MODEL_PRUNED(input_)       # inference
+        restored_image = MPRNet_MODEL_PRUNED_QUANTED(input_)       # inference
 
 
 
@@ -332,13 +495,14 @@ print("\n\nLog Result ...", end='\n\n')
 time_inference_MPRNet_avg = sum(time_inference_MPRnet) / len(time_inference_MPRnet)
 time_inference_YOLO_avg = sum(time_inference_YOLO) / len(time_inference_YOLO)
 
-print(summary(MPRNet_MODEL_PRUNED, input_size=(1, 3, 480, 320)))
-YOLO_MODEL_PRUNED.info()
+# print(summary(MPRNet_MODEL_PRUNED_QUANTED, input_size=(1, 3, 480, 320)))
+# count_model_param_flops(model=MPRNet_MODEL_PRUNED_QUANTED.eval(), input_res_width=480, input_res_height=320, multiply_adds=True)
+# YOLO_MODEL_PRUNED.info()
 
 print(f"Time Inference MPRNet {time_inference_MPRnet}")
 print(f"AVG :: {time_inference_MPRNet_avg} seconds.")
 print("\n\n\n")
-print(f"Time Inference MPRNet {time_inference_YOLO}")
+print(f"Time Inference YOLO {time_inference_YOLO}")
 print(f"AVG :: {time_inference_YOLO_avg} seconds.")
 
 
